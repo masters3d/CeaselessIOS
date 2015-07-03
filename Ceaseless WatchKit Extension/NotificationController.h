3e8e27a6b246fa4c62bcd7e144946a6a1d057016
LIL717 <lhill7@me.com>
2015-07-03 10:58:38 -0700

    watch kit

---
+++ Ceaseless WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  Ceaseless WatchKit Extension
+//
+//  Created by Lori Hill on 5/26/15.
+//  Copyright (c) 2015 Christopher Lim. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
